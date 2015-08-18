d106188cf9d6093886fc508b6ae2abb1dafa5bd6
Elie Marot <elie.marot@gmail.com>
2015-08-18 18:42:17 +0200

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+
+# get "tasks",            to: "tasks#index"
+# get "tasks/:id",        to: "tasks#show"
+# get "tasks/new",        to: "tasks#new"
+# post "tasks",           to: "tasks#create"
+# get "tasks/:id/edit",   to: "tasks#edit"
+# patch "tasks/:id",      to: "tasks#update"
+# delete "tasks/:id",     to: "tasks#destroy"
+
+resources :tasks
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
